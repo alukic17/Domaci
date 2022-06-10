533eec82486b86ca259d7c45e9209ce81d9eb435
alukic17 <107058369+alukic17@users.noreply.github.com>
2022-06-10 17:33:48 +0200

    Add files via upload

---
+++ client/request.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+url = "http://localhost:8081/users"
+
+payload = json.dumps({
+  "ime": "aca",
+  "prezime": "acic",
+  "smer": "RI",
+  "predmeti": [
+    {
+      "ime": "predmet1",
+      "espb": "5"
+    },
+    {
+      "ime": "predmet2",
+      "espb": "8"
+    }
+  ]
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
---
+++ client/run.sh
@@ -0,0 +1,1 @@
+docker build . -t client;docker run -d --network="host" client
